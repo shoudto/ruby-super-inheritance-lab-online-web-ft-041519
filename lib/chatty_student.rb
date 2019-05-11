4a67e3420ecce4dd07e6fef02ef9bb7f434b7d13
Oscar Shoudt <shoudto@gmail.com>
2019-05-11 17:35:47 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,9 +1,8 @@
 class ChattyStudent < Student 
   
   def hello
-   long_chatty_phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watchThe Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
    super  
-   puts long_chatty_phrase
+   puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watchThe Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
   def raise_hand 
