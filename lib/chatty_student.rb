d71f106722f69aa610b1b485a2f4b42ed806a8ed
Oscar Shoudt <shoudto@gmail.com>
2019-05-11 17:30:28 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,6 +2,7 @@ class ChattyStudent < Student
   
   def hello
     if super 
+      "How are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
   def raise_hand 
