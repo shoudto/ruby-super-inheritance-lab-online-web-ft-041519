28729f29e48809841cb57762102b214c08d6b228
Oscar Shoudt <shoudto@gmail.com>
2019-05-11 16:44:14 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,9 @@
 class Chatty_student 
   
-  attr_accessor :chatty_student 
-  def initialize(
-    @chatty_student = 
+  attr_accessor :chatty_student
+  
+  def initialize(chatty_student)
+    @chatty_student = chatty_student
   end 
+  
 end 
